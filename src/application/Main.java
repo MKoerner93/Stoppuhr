7894a4ead344b417c37349b63af864aa3ca33c40
MKoerner93 <koerner.programming@outlook.de>
2023-10-08 11:41:59 +0200

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author koern
+ */
+public class Main extends Application {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
